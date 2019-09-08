7d44e6a8415afa0f99e38d50da8247ae203725a1
shubham6350 <shubham.srivastava1_cs18@gla.ac.in>
2019-09-08 22:40:14 +0530

    Initial commit

---
+++ src/ps1/AddTwoNumbers.java
@@ -0,0 +1,11 @@
+package ps1;
+
+public class AddTwoNumbers {
+    public static void main(String[] args){
+        int num1=5,num2=15,sum;
+        sum=num1+num2;
+        System.out.println("sum of these numbers:"+sum);
+
+
+    }
+}
